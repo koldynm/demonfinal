5874de7e484b1454e8e0fd6e6240635edc45ccfc
unknown <koldynkoldyn7@gmail.com>
2025-04-11 17:12:54 +0300

    bobr

---
+++ demon3/Database.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace demon3;
+
+public class Item()
+{
+    public required  string Photo { get; set; }
+    public required string Name  { get; set; }
+    public required string Category  { get; set; }
+    public required string description  { get; set; }
+    public required int price  { get; set; }
+    public required float discount  { get; set; }
+}
+
+public class Database
+{
+    MySqlConnection demo=new MySqlConnection("server=localhost;port=3306;user=root;password=987654321;database=demo");
+    
+    public List<Item> getItems()
+    {
+        List<Item> items = new List<Item>();
+        demo.Open();
+        try
+        {
+            MySqlCommand cmd = new MySqlCommand(
+                "SELECT p.image, p.name as name, p.description, p.price, c.name as category, d.discount FROM products p  JOIN categories c ON p.cateigor = c.id  JOIN discounts d ON p.article = d.article ",
+                demo
+                );
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var item = new Item()
+                {
+                    Photo = reader["image"].ToString(),
+                    Name = reader["name"].ToString(),
+                    Category = reader["category"].ToString(),
+                    description = reader["description"].ToString(),
+                    price = int.Parse(reader["price"].ToString()),
+                    discount = float.Parse(reader["discount"].ToString())
+                };
+                items.Add(item);
+            }
+            reader.Close();
+        }
+        finally
+        {
+            demo.Close();
+        }
+        return items;
+    }
+}
---
+++ demon3/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demon3;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Database db = new Database();
+        
+        var itemdb = db.getItems();
+        string imageFolder = 
+            System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, 
+                "Resources"); 
+        string defaultImage = System.IO.Path.Combine(imageFolder, "picture.png"); 
+       
+
+        foreach (var dbdb in itemdb)
+        {
+            if (string.IsNullOrWhiteSpace(dbdb.Photo)) 
+            { 
+                dbdb.Photo = defaultImgage; 
+            }
+            else
+            {
+                dbdb.Photo  = System.IO.Path.Combine(imageFolder, dbdb.Photo);
+            }
+        }
+        
+        imia.ItemsSource = itemdb;
+    }
+}
